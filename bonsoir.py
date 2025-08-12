ba8745482a71ec301c29e93b6f207a177481bc32
jacques <jlefort251@gmail.com>
2025-08-12 17:28:40 +0200

    Ajout de l'image Severin

--- bonsoir.py
+++ bonsoir.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>Bonjour Jacques 👋</h1>"
+def bonsoir():
+    return "Bonsoir, monde !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
