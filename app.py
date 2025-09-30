51c749df02c2746b4946066f979c42ae292af385
lefort999 <jlefort251@gmail.com>
2025-09-30 18:59:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CHAPITRES = [
     "chapitre25","chapitre26","chapitre27","chapitre28",
     "chapitre29","chapitre30", "chapitre31", "chapitre32",
     "chapitre33","chapitre34","chapitre35","chapitre36",
-    "chapitre37",
+    "chapitre37","chapitre48","chapitre96",
 ]
 
 VALID_EXTS = {".jpg", ".jpeg", ".png", ".webp", ".gif"}
@@ -116,6 +116,7 @@ def afficher_chapitre(nom):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
